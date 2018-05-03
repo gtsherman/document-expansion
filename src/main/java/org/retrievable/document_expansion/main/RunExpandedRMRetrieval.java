8368cecc7b21c8f13408fffbf285ee4e6e78e4ff
Garrick Sherman <gsherma2@illinois.edu>
2018-05-03 14:57:55 -0700

    expanded RM now allows arbitrary number of expansion indexes
    In keeping with past decisions, the optimal interpolation weights from
    the non-RM run are used. OptimalParameters is therefore updated to
    record expWeights. The primary functional change is in
    ExpandedRM1Builder which now allows for multiple ExpansionDocScorers in
    each InterpolatedDocScorer.

--- src/main/java/org/retrievable/document_expansion/main/RunExpandedRMRetrieval.java
+++ src/main/java/org/retrievable/document_expansion/main/RunExpandedRMRetrieval.java
@@ -21,9 +21,11 @@ import org.retrievable.document_expansion.expansion.DocumentExpander;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.OutputStreamWriter;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class RunExpandedRMRetrieval {
 
@@ -38,7 +40,9 @@ public class RunExpandedRMRetrieval {
 		Stopper stopper = new Stopper(config.getString("stoplist"));
 
 		IndexWrapper targetIndex = new CachedFeatureVectorIndexWrapperIndriImpl(config.getString("target-index"));
-		IndexWrapper expansionIndex = new CachedFeatureVectorIndexWrapperIndriImpl(config.getString("expansion-index"));
+        List<IndexWrapper> expansionIndexes = Arrays.stream(config.getStringArray("expansion-index"))
+                .map(expInd -> new CachedFeatureVectorIndexWrapperIndriImpl(expInd))
+                .collect(Collectors.toList());
 
 		GQueries queries = GQueriesFactory.getGQueries(config.getString("queries"));
 		GQuery query = queries.getNamedQuery(queryName);
@@ -57,15 +61,20 @@ public class RunExpandedRMRetrieval {
 		int maxFbTerms = Integer.parseInt(config.getString("max-fbterms", "50"));
 		int fbTermsInterval = Integer.parseInt(config.getString("fbterms-interval", "10"));
 
-		DocumentExpander docExpander = new DocumentExpander(expansionIndex, expansionParams.getNumTerms(), stopper);
+        List<DocumentExpander> docExpanders = expansionIndexes
+                .stream()
+                .map(expansionIndex -> new DocumentExpander(expansionIndex, expansionParams.getNumTerms(), stopper))
+                .collect(Collectors.toList());
 
 		// Get the feedback docs
 		query.applyStopper(stopper);
 		SearchHits feedbackDocs = targetIndex.runQuery(query, maxFbDocs); // get max fbDocs; we'll trim as we go along
 
 		// Prep RM builders
-		ExpandedRM1Builder rm1Builder = new ExpandedRM1Builder(maxFbDocs, maxFbTerms, targetCollectionStats, docExpander, expansionParams.getNumDocs());
-		rm1Builder.setOriginalLMWeight(expansionParams.getOrigWeight());
+        ExpandedRM1Builder rm1Builder = new ExpandedRM1Builder(maxFbDocs, maxFbTerms, targetCollectionStats, docExpanders, expansionParams.getNumDocs());
+        List<Double> interpolationWeights = expansionParams.getExpWeights();
+        interpolationWeights.add(0, expansionParams.getOrigWeight());
+        rm1Builder.setInterpolationWeights(interpolationWeights);
 		RM3Builder rm3Builder = new RM3Builder();
 
 		// Prep the RM3 query
@@ -81,39 +90,41 @@ public class RunExpandedRMRetrieval {
         for (int fbDocs = maxFbDocs; fbDocs >= minFbDocs; fbDocs -= fbDocsInterval) {
             rm1Builder.setFeedbackDocs(fbDocs);
 
-            //for (int origWeightInt = 0; origWeightInt <= 10; origWeightInt++) {
-            //    double origWeight = origWeightInt / 10.0;
+            FeatureVector rm1Vector = rm1Builder.buildRelevanceModel(query, feedbackDocs, stopper);
 
-                FeatureVector rm1Vector = rm1Builder.buildRelevanceModel(query, feedbackDocs, stopper);
+            for (int fbTerms = maxFbTerms; fbTerms >= minFbTerms; fbTerms -= fbTermsInterval) {
+                // As with fbDocs, work backwards through fbTerms
+                rm1Vector.clip(fbTerms);
 
-                for (int fbTerms = maxFbTerms; fbTerms >= minFbTerms; fbTerms -= fbTermsInterval) {
-                    // As with fbDocs, work backwards through fbTerms
-                    rm1Vector.clip(fbTerms);
+                for (int fbOrigWeightInt = 0; fbOrigWeightInt <= 10; fbOrigWeightInt++) {
+                    double fbOrigWeight = fbOrigWeightInt / 10.0;
 
-                    for (int fbOrigWeightInt = 0; fbOrigWeightInt <= 10; fbOrigWeightInt++) {
-                        double fbOrigWeight = fbOrigWeightInt / 10.0;
+                    // Build the RM3
+                    FeatureVector rm3 = rm3Builder.buildRelevanceModel(query, rm1Vector, fbOrigWeight);
 
-                        // Build the RM3
-                        FeatureVector rm3 = rm3Builder.buildRelevanceModel(query, rm1Vector, fbOrigWeight);
+                    // Set the RM3 as the query
+                    rm3Query.setFeatureVector(rm3);
 
-                        // Set the RM3 as the query
-                        rm3Query.setFeatureVector(rm3);
+                    // Execute the RM3 query
+                    SearchHits results = targetIndex.runQuery(rm3Query, 1000);
 
-                        // Execute the RM3 query
-                        SearchHits results = targetIndex.runQuery(rm3Query, 1000);
-
-                        // Write results
-                        out.setRunId("origW:" + expansionParams.getOrigWeight() +
-                                        ",expDocs:" + expansionParams.getNumDocs() +
-                                        ",expTerms:" + expansionParams.getNumTerms() +
-                                        ",fbOrigWeight:" + fbOrigWeight +
-                                        ",fbDocs:" + fbDocs +
-                                        ",fbTerms:" + fbTerms
-                        );
-                        out.write(results, query.getTitle());
+                    // Write results
+                    String expWeightLabels = "";
+                    for (int i = 1; i < interpolationWeights.size(); i++) {
+                        expWeightLabels += "expW" + i + ":" + interpolationWeights.get(i);
                     }
+                    out.setRunId(
+                            expWeightLabels +
+                                    ",origW:" + expansionParams.getOrigWeight() +
+                                    ",expDocs:" + expansionParams.getNumDocs() +
+                                    ",expTerms:" + expansionParams.getNumTerms() +
+                                    ",fbOrigWeight:" + fbOrigWeight +
+                                    ",fbDocs:" + fbDocs +
+                                    ",fbTerms:" + fbTerms
+                    );
+                    out.write(results, query.getTitle());
                 }
-            //}
+            }
         }
 	}
 }
